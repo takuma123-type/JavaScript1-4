c4b593f3f53c693e45e67df9c88dd1287fdfc145
takuma123-type <72244249+takuma123-type@users.noreply.github.com>
2020-10-23 18:04:15 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+//問１
+let a = 10;
+let b = 12;
+if (a >=10 != a <= 10);
+if (b >= 10 && b <= 20) {
+} else if(b % 2 === 0){
+}
+
+
+
+//問２
+let x = 10;
+if (a >= 10 && a <= 20) { //変数Aが１０から２０の時
+   console.log("成功です");
+} else {
+   console.log("失敗です");
+}
+
+
+//問３
+let y = 0;
+if (y % 2 === 0) {
+   console.log ("偶数");
+}
+
+if (b % 2 !== 0) {
+   console.log("奇数");
+}
